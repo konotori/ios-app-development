06077d8bc9ba9211050cc78bea1e35da314a0401
Tung Nguyen Viet <tungto2010@gmail.com>
2019-11-01 10:27:53 +0700

    [Guided Project] Unit2: ApplePie

---
+++ ApplePie/ApplePie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Tung Nguyen on 10/30/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter){
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+        print(guessedLetters)
+        print(formattedWord)
+    }
+}
+
