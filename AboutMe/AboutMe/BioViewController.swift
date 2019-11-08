e7333d6150d6dd8e416fe716dac3e5f18f8f01b2
Tung Nguyen Viet <tungto2010@gmail.com>
2019-11-08 17:33:02 +0700

    [Lab 4] Unit3: AboutMe

---
+++ AboutMe/AboutMe/BioViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Tung Nguyen on 11/7/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AboutMe/AboutMe/HobbiesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HobbiesViewController.swift
+//  AboutMe
+//
+//  Created by Tung Nguyen on 11/7/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class HobbiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
