dd798733bd61284ac3c3358fe403efda5f7c1c9a
Tung Nguyen Viet <tungto2010@gmail.com>
2019-11-08 17:32:19 +0700

    [Lab 3] Unit3: RainBowTab

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Tung Nguyen on 11/7/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = "!"
+    }
+
+
+}
+
