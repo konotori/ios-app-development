959d423286d0c22e0b2efd184478dcfdbdc86c16
Tung Nguyen Viet <tungto2010@gmail.com>
2019-11-22 17:16:23 +0700

    [Lab 2] Unit 2: Hobby Tutorial

---
+++ HobbyTutorial/HobbyTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTutorial
+//
+//  Created by Tung Nguyen on 11/22/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
