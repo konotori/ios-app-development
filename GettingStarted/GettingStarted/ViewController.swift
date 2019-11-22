98282f577e5adcac074dbbf210475055b32cdb50
Tung Nguyen Viet <tungto2010@gmail.com>
2019-11-22 16:51:57 +0700

    [Lab 1] Unit 1: Getting Started

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Tung Nguyen on 11/22/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+
+}
+
