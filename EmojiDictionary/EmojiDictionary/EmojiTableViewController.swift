1bc3688cc71c84519f5f53cd527e54c8cfb1e12c
Tung Nguyen Viet <tungto2010@gmail.com>
2019-12-03 15:24:15 +0700

    Adding saving function

--- EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -10,34 +10,20 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    var emojis: [Emoji] = [
-       Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
-       Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-       Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
-       Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a goldbadge.", usage: "person of authority"),
-       Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
-       Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
-       Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
-       Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-       Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-       Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
-       Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"), Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-       Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage:
-          "completion")]
+    var emojis: [Emoji] = []
       
-    
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight  = 44.0
+        
+        if let savedEmojis = Emoji.loadFromFile() {
+            emojis = savedEmojis
+        } else {
+            emojis = Emoji.loadSampleEmojis()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -144,6 +130,8 @@ class EmojiTableViewController: UITableViewController {
                 emojis.append(emoji)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            
+            Emoji.saveToFile(emojis: emojis)
         }
     }
 
